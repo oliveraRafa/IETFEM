6e6e9320ab722fd24e71c0817a20785490cb18fc
rafa <rafael.olivera.399@gmail.com>
2015-08-18 23:50:04 -0300

    Angular migration

---
+++ UI/app/main.controller.js
@@ -0,0 +1,265 @@
+var app = angular.module('IETFEM', []);
+app.controller(
+    'mainCtrl',
+	[	'$scope',
+		'ModelService',
+		'SpaceService',
+        function($scope, ModelService, SpaceService){
+		
+			//--- Defino función de inicialización
+			function init() {
+
+				//Obtengo el viewport (donde se dibuja)
+				viewport = document.getElementById( 'viewport' );
+				
+				viewportWidth=$("#viewportContainer").width();
+				viewportHeight=(window.innerHeight-46);
+				
+				//Seteo la camara
+				camera = new THREE.PerspectiveCamera( 60, viewportWidth / viewportHeight, 1, 1000 );
+				camera.position.y = 10;
+
+				//Asigno controles de la camara
+				controls = new THREE.OrbitControls( camera, viewport );
+				controls.damping = 0.1;
+				controls.addEventListener( 'change', render );
+
+				//Creo la escena dentro del viewport, pongo grilla auxiliar
+				scene = new THREE.Scene();
+				grid = new THREE.GridHelper( 1000, 1 );
+				grid.setColors( new THREE.Color(0x838383), new THREE.Color(0xD0D0D0) );
+				grid.position.set(0,0,0);
+				scene.add(grid);
+				
+				// Seteo el renderer(manejador de objetos en la escena)
+
+				renderer = new THREE.WebGLRenderer( { antialias: false } );
+				renderer.setPixelRatio( window.devicePixelRatio );
+				renderer.setSize( viewportWidth, viewportHeight );
+				renderer.setClearColor( 0xEEEEEE, 1 );
+
+				viewport.appendChild( renderer.domElement );
+
+				//Agrego eventos del usuario
+				window.addEventListener( 'resize', onWindowResize, false );
+				window.addEventListener( 'mouseup', onMouseUp, false );
+				window.addEventListener( 'mousedown', onMouseDown, false );
+				
+				//Agrego el origen
+				$scope.addParticle(0,0,0);
+
+			}
+
+			//--- Defino función de Render
+			function render() {
+
+				renderer.render( scene, camera );
+					
+			}  
+			
+			//--- Defino Eventos de usuario
+			
+			//Cuando se cambia el tamaño de la ventana
+			function onWindowResize() {
+				
+				viewportWidth=$("#viewportContainer").width();
+				viewportHeight=(window.innerHeight-46);
+
+				camera.aspect = viewportWidth / viewportHeight;
+				camera.updateProjectionMatrix();
+
+				renderer.setSize( $("#viewportContainer").width(), window.innerHeight-46 );
+
+				render();
+
+			}
+
+			//Cuando se presiona el click izquierdo
+			function onMouseDown( event ) {
+				if (event.button == 0){
+					mouseX = event.clientX;
+					mouseY = event.clientY;
+				}
+			}
+			
+			//Pone la informacion del punto en los controles de la UI
+			$scope.getInfoPuntoInterfaz = function() {
+				  $scope.puntoId = puntoSeleccionado.id;
+				  $scope.xCondition =puntoSeleccionado.xCondition;
+			}
+			//Cuando se levanta el click izquierdo
+			function onMouseUp( event ) {  
+				
+				viewportWidth=$("#viewportContainer").width();
+				viewportHeight=(window.innerHeight-46);
+				
+				if ((event.button == 0) && (mouseX == event.clientX) && (mouseY == event.clientY)){
+
+				 var vector = new THREE.Vector3( ( 
+					event.clientX / viewportWidth) * 2 - 1, 
+					- ( (event.clientY-46) / (viewportHeight) ) * 2 + 1, 
+					0.5 
+				);
+				
+				vector.unproject( camera );
+
+				var ray = new THREE.Raycaster( camera.position, vector.sub( camera.position ).normalize() );
+				
+				//FUNCION OBTENGO punto seleccionado
+				var pointIntersection = ray.intersectObjects(puntosEscena);
+				
+				if(pointIntersection.length > 0){
+					puntoSeleccionado= ModelService.getPointById(pointIntersection[0].object.id,model);
+					//Salert(puntoSeleccionado.sceneId);
+				}
+				//----------------------------------
+				var intersects = ray.intersectObjects( helpObjects );
+				if ( intersects.length > 0 ) {
+					
+					if (!addingLines){
+						//Agrego el punto
+						intersects[0].object.material = new THREE.MeshBasicMaterial( {color: 0x000000} );
+						ModelService.addPointToModel(intersects[0].object.position.x, intersects[0].object.position.y, intersects[0].object.position.z, intersects[0].object.id, model);
+						puntosEscena.push(intersects[0].object);	
+					} else {
+						if (ModelService.isInModel(intersects[0].object.position.x, intersects[0].object.position.y,intersects[0].object.position.z, model)){
+							if (firstPointLine == null){
+								firstPointLine = {};
+								firstPointLine.x = intersects[0].object.position.x;
+								firstPointLine.y = intersects[0].object.position.y;
+								firstPointLine.z = intersects[0].object.position.z;
+								idFirstPoint = intersects[0].object.id;
+								intersects[0].object.material = new THREE.MeshBasicMaterial( {color: 0x8B0000} );
+								intersects[0].object.geometry = new THREE.SphereGeometry( 0.1, 50, 50 );
+								
+							} else {
+								SpaceService.drawLine(firstPointLine.x, firstPointLine.y, firstPointLine.z, intersects[0].object.position.x, intersects[0].object.position.y, intersects[0].object.position.z, new THREE.LineBasicMaterial({color: 0x000000}), 0.015, scene);
+								ModelService.addLineToModel(firstPointLine.x, firstPointLine.y, firstPointLine.z, intersects[0].object.position.x, intersects[0].object.position.y, intersects[0].object.position.z, model);
+								SpaceService.getScenePointById(idFirstPoint, scene).material = new THREE.MeshBasicMaterial( {color: 0x000000} );
+								SpaceService.getScenePointById(idFirstPoint, scene).geometry = new THREE.SphereGeometry( 0.05, 50, 50 );
+								idFirstPoint = 0;
+								firstPointLine = null;
+							}
+						}
+					}
+				}
+				
+				render();
+				}
+			}
+			
+			// Funciones principales accesadas desde la view
+			
+			//Agrega una grilla
+			$scope.addGrid = function() {
+						
+				var line, geometry, i, j;
+
+				var material = new THREE.LineBasicMaterial({color: 0xFF0000, transparent: true, opacity: 0.15});
+				
+				for (i=0; i < $scope.largoY * $scope.separatorY ; i = i + $scope.separatorY){
+					for (j=0; j < $scope.largoX * $scope.separatorX; j = j + $scope.separatorX){
+						SpaceService.drawLine($scope.posX + j, $scope.posY + i, $scope.posZ, $scope.posX + j, $scope.posY + i, $scope.posZ + ($scope.largoZ - 1) * $scope.separatorZ, material, 0.01, scene)
+					}
+					for (j=0; j < $scope.largoZ * $scope.separatorZ; j = j + $scope.separatorZ){
+						SpaceService.drawLine($scope.posX, $scope.posY + i, $scope.posZ + j, $scope.posX + ($scope.largoX - 1) * $scope.separatorX, $scope.posY + i, $scope.posZ + j, material, 0.01, scene)
+						for (k=0; k < $scope.largoX * $scope.separatorX; k = k + $scope.separatorX){
+							var sphereGeometry = new THREE.SphereGeometry( 0.05, 10, 10 );
+							var sphereMaterial = new THREE.MeshBasicMaterial( {color: 0xFF0000, transparent: true, opacity: 0.15} );
+							var sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
+							sphere.position.x = $scope.posX+k
+							sphere.position.y = $scope.posY+i
+							sphere.position.z = $scope.posZ+j
+							scene.add(sphere);
+							helpObjects.push(sphere);
+							if (i > 0){
+								SpaceService.drawLine($scope.posX + k, $scope.posY + i - $scope.separatorY, $scope.posZ + j, $scope.posX + k, $scope.posY + i, $scope.posZ + j, material, 0.01, scene)
+							}
+						}
+					}
+				}
+
+				render()
+			
+			};
+			
+			//Agrega un punto
+			$scope.addPoint = function(){
+				SpaceService.drawPoint($scope.posX, $scope.posY, $scope.posZ, scene, puntosEscena, helpObjects);
+				
+				var sceneId = SpaceService.getScenePointIdByCoords($scope.posX, $scope.posY, $scope.posZ, scene);
+				ModelService.addPointToModel($scope.posX, $scope.posY, $scope.posZ, sceneId, model);
+				render();
+			};
+			
+			//Agrega una partícula
+			$scope.addParticle = function(x,y,z){
+				var particleGeometry  = new THREE.Geometry;
+				var point = new THREE.Vector3(x,y,z);
+				particleGeometry.vertices.push(point);
+				var particleMaterial = new THREE.PointCloudMaterial({color: 0x000000,size: 0.2});
+				var particle = new THREE.PointCloud(particleGeometry, particleMaterial);	
+				scene.add(particle);
+				render();
+			},
+			
+			$scope.alternateLinesPoints = function(){
+				if (addingLines) {
+					addingLines = false;
+					firsPointLine = null;
+					$scope.linesButton = "Agregar líneas"
+				} else {
+					addingLines = true;
+					$scope.linesButton = "Agregar puntos"
+				}
+			};
+			
+			//Setea vista 2D
+			$scope.set2D = function(){
+				tridimensional = false;
+				camera.position.x = 0;		
+				camera.position.y = 10;		
+				camera.position.z = 0;		
+				camera.lookAt(new THREE.Vector3(0,0,0));
+				controls.removeEventListener( 'change', render );
+				render();				
+			},
+					
+			//Setea vista 3D
+			$scope.set3D = function (){
+				tridimensional = true;
+				camera.position.x = 5;
+				camera.position.y = 10;
+				camera.position.z = 5;	
+				camera.lookAt(new THREE.Vector3(0,0,0));				
+				controls.addEventListener( 'change', render );
+				render();				
+			}
+			
+			$scope.modelToText = function(){
+				$scope.modelText = ModelService.getText(model)
+			}
+			
+			// --- Inicializa variables
+			var viewport, viewportWidth, viewportHeight, puntoSeleccionado;	
+			var camera, controls, scene, renderer, tridimensional, grid;
+			var mouseX,  mouseY;
+			
+			var addingLines = false;
+			var firstPointLine = null;
+			var idFirstPoint = 0;
+			var helpObjects = [];
+			var puntosEscena = [];
+			var model = {};
+			model.points = [];
+			model.lines = [];
+			
+			$scope.linesButton = "Agregar líneas"
+			
+			// --- Inicializa escena
+			
+			init();
+			render();
+			
+		}
+	]);
---
+++ UI/app/services/model.service.js
@@ -0,0 +1,106 @@
+angular.module('IETFEM')
+.factory('ModelService', function(SpaceService) {
+
+	// Funciones internas
+
+	//Genera un nuevo identificador
+	var newIdentifier = function(type, model) {
+		var id = 0;
+		if (type == 'POINT'){
+			for (var i = 0; i < model.points.length; i++){
+				if (model.points[i].id > id)
+					id = model.points[i].id
+			}
+		} else { 
+			for (var i = 0; i < model.lines.length; i++){
+				if (model.lines[i].id > id)
+					id = model.lines[i].id
+			}					
+		}
+		return id+1;
+	};
+	
+	//Obtiene un punto del modelo dadas sus coordenadas
+	var getPointIdByCoords = function(x,y,z,model) {
+		var id = 0;
+		for (var i = 0; i < model.points.length ;i++){
+			if (model.points[i].coords.x == x && model.points[i].coords.y == y && model.points[i].coords.z == z)
+				id = model.points[i].id;
+		};
+		return id;
+	};
+	//--- Fin internas
+	
+	//Obtener objeto por id
+	var getPointById = function(id, model) {
+			
+		for (var i = 0; i < model.points.length ;i++){
+			if (model.points[i].sceneId==id){
+				return model.points[i];
+			}
+		};
+	};
+	
+	//Agrega un punto al modelo
+	var addPointToModel = function(pX,pY,pZ, sceneId, model) {
+			
+		if (pX != 0 && pY != 0 && pZ != 0){
+		var point = {};
+		point.id = newIdentifier('POINT', model);
+		point.sceneId = sceneId;
+		//Displacement conditions
+		point.xCondition=0;
+		point.coords = {
+			x: pX,
+			y: pY,
+			z: pZ
+		};
+		model.points.push(point);
+		}
+	};
+	
+	//Agrega una línea al modelo
+	var addLineToModel = function(pX1, pY1, pZ1, pX2, pY2, pZ2, model) {		
+		var line = {};
+		line.id = newIdentifier('LINE', model);
+		line.properties = [];
+		line.start = getPointIdByCoords(pX1, pY1, pZ1,model);
+		line.end = getPointIdByCoords(pX2, pY2, pZ2, model);
+		
+		model.lines.push(line);
+	};
+	
+	//Verifica si un punto está en el modelo
+	var isInModel = function(x,y,z, model) {
+		var drawed = true;
+		for (var i = 0; i < model.points.length ;i++){
+			if (model.points[i].coords.x == x && model.points[i].coords.y == y && model.points[i].coords.z == z)
+				return true
+		};
+		return false;
+	};
+	
+	//Genera txt a partir del modelo
+	var getText = function(model) {
+		var text;
+		text = 'Node matrix ' + '\n';
+		text += 'Id     Xs     Ys     Zs' + '\n';
+		for (var i = 0; i < model.points.length ;i++){
+			text += model.points[i].id + '     ' + model.points[i].coords.x + '     ' + model.points[i].coords.y + '     ' + model.points[i].coords.z + '\n'; 
+		}
+		text += '\n' + 'Conectivity matrix' + '\n';
+		text += 'material     section     tempcase     start     end' + '\n';
+		for (var i = 0; i < model.lines.length ;i++){
+			text += 1 + '     ' + 1 + '     ' + 0 + '     ' + model.lines[i].start + '     ' + model.lines[i].end + '\n'; 
+		}
+		return text;
+	};
+
+	return {
+		getPointById: getPointById,
+		addPointToModel: addPointToModel,
+		addLineToModel: addLineToModel,
+		isInModel: isInModel,
+		getText: getText,
+	};
+});
