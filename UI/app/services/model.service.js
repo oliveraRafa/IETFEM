84692db339565b01b41d1c002f27875b03860092
rafa <rafael.olivera.399@gmail.com>
2015-09-02 23:22:28 -0300

    creacion de txt

--- UI/app/services/model.service.js
+++ UI/app/services/model.service.js
@@ -122,17 +122,66 @@ angular.module('IETFEM')
 	//Genera txt a partir del modelo
 	var getText = function(model) {
 		var text;
-		text = 'Node matrix ' + '\n';
-		text += 'Id     Xs     Ys     Zs' + '\n';
+		console.log(model);
+		text = 'Input for a 3D with large deformation (You must respect line breaks):' + '\n\n' + 'Force Magnitude' + '\n' + 'N' + '\n\n' + 'Length Magnitude' + '\n' + 'm' + '\n\n'
+			   + 'Number of degrees of freedom per node' + '\n' + '3' + '\n\n' + 'Number of nodes per element' + '\n' + '2' + '\n\n';
+
+		text +=  'Number of materials' + '\n' + model.materiales.length + '\n\n';
+		
+		text += 'Materials:' + '\n' + 'Young Modulus        Gamma         alpha (1/C)';
+		for (var i = 0; i < model.materiales.length ;i++){
+			text += model.materiales[i].youngModule + '     ' + model.materiales[i].gamma + '     ' + model.materiales[i].alpha + '\n'; 
+		}
+		text += '\n' + 'Number of temperature cases' + '\n' + '0' + '\n\n' + 'Temperature cases:' + '\n' + 'Value' + '\n\n';
+		
+		text +=  'Number of sections' + '\n' + model.secciones.length + '\n\n';
+
+		text += 'Sections:' + '\n' + 'Area' + '\n';
+		for (var i = 0; i < model.secciones.length ;i++){
+			text += model.secciones[i].section + '\n'; 
+		}
+		
+		text += '\n' + 'Number of nodes' + '\n' + model.points.length + '\n\n';
+		
+		text += 'Node matrix ' + '\n';
+		text += 'Xs     Ys     Zs' + '\n';
 		for (var i = 0; i < model.points.length ;i++){
-			text += model.points[i].id + '     ' + model.points[i].coords.x + '     ' + model.points[i].coords.y + '     ' + model.points[i].coords.z + '\n'; 
+			text += model.points[i].coords.x + '     ' + model.points[i].coords.y + '     ' + model.points[i].coords.z + '\n'; 
 		}
 		text += '\n' + 'Conectivity matrix' + '\n';
 		text += 'material     section     tempcase     start     end' + '\n';
 		for (var i = 0; i < model.lines.length ;i++){
-			text += 1 + '     ' + 1 + '     ' + 0 + '     ' + model.lines[i].start + '     ' + model.lines[i].end + '\n'; 
+			text += model.lines[i].material + '     ' + model.lines[i].section + '     ' + 0 + '     ' + model.lines[i].start + '     ' + model.lines[i].end + '\n'; 
 		}
+		var displacementNodes = [];
+		for (var i = 0; i < model.points.length ;i++){
+			if (model.points[i].xCondicion == 'F' || model.points[i].yCondicion == 'F' || model.points[i].zCondicion == 'F'){
+				displacementNodes.add(model.points[i]);
+			}
+		}
+		text += '\n' + 'Number of displacement conditions nodes' + '\n' + displacementNodes.length + '\n' + 'Load node       FX            FY           FZ' + '\n'
+		for (var i = 0; i < displacementNodes.length ;i++){
+			text += i + '     ' + loadNodes[i].xForce + '     ' + loadNodes[i].yForce + '     ' + loadNodes[i].zForce + '\n'; 
+		}
+		
+		var loadNodes = [];
+		for (var i = 0; i < model.points.length ;i++){
+			if (model.points[i].xFrorce == 0 || model.points[i].yForce == 0 || model.points[i].zForce == 'F'){
+				loadNodes.add(model.points[i]);
+			}
+		}
+		text += '\n' + 'vNumber of displacement conditions nodes' + '\n' + loadNodes.length + '\n' + 'Displacement node  X condition   Y condition   Z condition' + '\n'
+		for (var i = 0; i < displacementNodes.length ;i++){
+			text += i + '     ' + displacementNodes[i].xCondicion + '     ' + displacementNodes[i].yCondicion + '     ' + displacementNodes[i].zCondicion + '\n'; 
+		}
+		
+		
+		//Agrega las opciones de salida al final
+		text += 'Number of dead volume load conditions' + '\n' + '0' +'\n\n' + 'Dead volume loads conditions matrix' + '\n' + 'Element           bx                  by                  bz' + '\n\n' + 'Number of springs conditions nodes' + '\n' + '0' + '\n\n' + 'Springs conditions nodes matrix' + '/n' + 'Spring node  X condition   Y condition   Z condition' + '\n\n' + 'Scale Factor' + '\n' + 'SD_Deformed   Supports    Areas    Forces    Frames    Numbers' + '\n' + '   70           1         1         1         0.05         1' + '\n\n' + 'What you wanna see? (Yes=1, No=0)' + '\n' + 'Indeformed   SD_Deformed   SD_Axial' + '\n' + '1                 1            1' + '\n\n' + 'Wich of the plots selected above do you want to print (.png image)? (Yes=1, No=0)' + '\n' + 'Indeformed   SD_Deformed  SD_Axial' + '\n' + '1                 1            1' + '\n\n' + 'How many images do you wanna see for small deformation?' + '\n' + 'SD_Deformed   SD_Axial' + '\n' + '1                 1            1' + '\n\n' + 'What you wanna see in plots? (Yes=1, No=0)' + '\n' + 'Supports   Node_Numbers   Elements_Numbers   Forces          Axial_Force_Value' + '1               0                0              1                   0' + '\n\n' + 'For 3D plots, what you wanna see? (Yes=1, No=0)' + '\n' + 'If you choose Dif_View=1 IETFEM use default AZIMUTH and ELEVATION, if you choose Dif_View=0 you must type aximuth and elevation in degrees.' + '\n' + 'XY_plane    XZ_plane    YZ_plane    Dif_View   AZIMUTH(degree)   ELEVATION(degree)' + '\n' + '1            1           1           0           150               15' + '\n\n' + 'Text output format (Yes=1, No=0)' + '\n' + 'TXT  TEX' + '\n' + '1     1' + '/n';
+
+		//window.open("data:text/json;charset=utf-8," + escape("Ur String Object goes here"));
 		return text;
+		
 	};
 
 	return {
