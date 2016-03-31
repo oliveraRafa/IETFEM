213ab9988bcf3dc9e770a2e2410a2c4ea5169e52
Federico <fedegarciarod@gmail.com>
2016-03-31 18:05:25 -0300

    Arreglado factor de escala del output

--- UI/app/controllers/editOutput.controller.js
+++ UI/app/controllers/editOutput.controller.js
@@ -1,6 +1,8 @@
 var app = angular.module('IETFEM');
 	app.controller('editOutputCtrl',['$scope','ModelService', 'DeformedService', function($scope, ModelService, DeformedService){
 	
+	$scope.scaleStructureAux=1;
+
 	$scope.$watch('structureView', function() {
 	    if ($scope.structureView === 'normal'){
 	    	ModelService.setModelOpaque($scope.scene, $scope.model);
@@ -26,7 +28,24 @@ var app = angular.module('IETFEM');
 		}
 	    ModelService.colorizeModel($scope.scene, $scope.model, $scope.deformed, $scope.structureColors, $scope.structureView != 'normal');
 	    $scope.render();
-	});	
+	});
+
+	$scope.toggleScaleStructure = function(isRange){
+		//Arreglos para slider con distinta escala!! Utilizo valor auxiliar en el range y ajusto el factor de escala real acorde
+		if(!isNaN(parseFloat($scope.scaleStructure))){
+			$scope.scaleStructure=parseFloat($scope.scaleStructure);
+		}
+		if(isRange){
+			
+				$scope.scaleStructure=$scope.scaleStructureAux;
+			
+		}else{
+				if($scope.scaleStructure > 1000){
+					$scope.scaleStructure=1000;
+				}
+				$scope.scaleStructureAux=$scope.scaleStructure;
+		}
+	}	
 
 
 	}]);
